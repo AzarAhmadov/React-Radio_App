c685f3fa4ea3860f62406f233b7ad7e74423303f
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2022-12-18 14:42:40 -0800

    project finish

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { DATA } from './Data';
+import { useState } from 'react';
+
+function App() {
+
+  const [url, setUrl] = useState(null);
+
+  const getLink = () => {
+    setUrl(url)
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1> Turkish Radio Station  </h1>
+      </header>
+
+      <div className="radio-flex">
+        {
+          DATA.map((item, id) => {
+            return (
+              <div key={id} onClick={() => setUrl(item.url)} className="radio-images">
+                <img src={item.image} />
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <div className="audio-player">
+        <AudioPlayer
+          autoPlay
+          src={url}
+          onPlay={e => console.log("onPlay")}
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
